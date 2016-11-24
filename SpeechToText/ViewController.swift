d68aa298677ff034546d28788b26802ff42c3b60
satoshin2071 <satoshin2071@gmail.com>
2016-11-24 19:22:23 +0900

    Updated for Swift 3.0

--- SpeechToText/ViewController.swift
+++ SpeechToText/ViewController.swift
@@ -31,7 +31,7 @@ class ViewController: UIViewController, OEEventsObserverDelegate {
         loadOpenEars()
     }
     
-    @IBAction func record(sender: AnyObject) {
+    @IBAction func record(_ sender: AnyObject) {
         
         if !buttonFlashing {
             startFlashingbutton()
@@ -47,24 +47,18 @@ class ViewController: UIViewController, OEEventsObserverDelegate {
         buttonFlashing = true
         recordButton.alpha = 1
         
-        UIView.animateWithDuration(0.5 , delay: 0.0, options: UIViewAnimationOptions.CurveEaseInOut | UIViewAnimationOptions.Repeat | UIViewAnimationOptions.Autoreverse | UIViewAnimationOptions.AllowUserInteraction, animations: {
-            
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: [.curveEaseInOut, .repeat, .autoreverse], animations: { [unowned self] in
             self.recordButton.alpha = 0.1
-
-            }, completion: {Bool in
-        })
+            }, completion: nil)
     }
 
     func stopFlashingbutton() {
  
         buttonFlashing = false
         
-        UIView.animateWithDuration(0.1, delay: 0.0, options: UIViewAnimationOptions.CurveEaseInOut | UIViewAnimationOptions.BeginFromCurrentState, animations: {
-
+        UIView.animate(withDuration: 0.1, delay: 0.0, options: [.curveEaseInOut,.beginFromCurrentState], animations: { [unowned self] in
             self.recordButton.alpha = 1
-
-            }, completion: {Bool in
-        })
+            }, completion: nil)
     }
     //OpenEars methods begin
     
@@ -73,69 +67,74 @@ class ViewController: UIViewController, OEEventsObserverDelegate {
         self.openEarsEventsObserver = OEEventsObserver()
         self.openEarsEventsObserver.delegate = self
 
-        var lmGenerator: OELanguageModelGenerator = OELanguageModelGenerator()
+        let lmGenerator: OELanguageModelGenerator = OELanguageModelGenerator()
 
         addWords()
-        var name = "LanguageModelFileStarSaver"
-        lmGenerator.generateLanguageModelFromArray(words, withFilesNamed: name, forAcousticModelAtPath: OEAcousticModel.pathToModel("AcousticModelEnglish"))
+        let name = "LanguageModelFileStarSaver"
+        lmGenerator.generateLanguageModel(from: words, withFilesNamed: name, forAcousticModelAtPath: OEAcousticModel.path(toModel: "AcousticModelEnglish"))
         
-        lmPath = lmGenerator.pathToSuccessfullyGeneratedLanguageModelWithRequestedName(name)
-        dicPath = lmGenerator.pathToSuccessfullyGeneratedDictionaryWithRequestedName(name)
+        lmPath = lmGenerator.pathToSuccessfullyGeneratedLanguageModel(withRequestedName: name)
+        dicPath = lmGenerator.pathToSuccessfullyGeneratedDictionary(withRequestedName: name)
     }
 
 
     func pocketsphinxDidStartListening() {
-        println("Pocketsphinx is now listening.")
+        print("Pocketsphinx is now listening.")
         statusTextView.text = "Pocketsphinx is now listening."
     }
     
     func pocketsphinxDidDetectSpeech() {
-        println("Pocketsphinx has detected speech.")
+        print("Pocketsphinx has detected speech.")
         statusTextView.text = "Pocketsphinx has detected speech."
     }
     
     func pocketsphinxDidDetectFinishedSpeech() {
-        println("Pocketsphinx has detected a period of silence, concluding an utterance.")
+        print("Pocketsphinx has detected a period of silence, concluding an utterance.")
         statusTextView.text = "Pocketsphinx has detected a period of silence, concluding an utterance."
     }
     
     func pocketsphinxDidStopListening() {
-        println("Pocketsphinx has stopped listening.")
+        print("Pocketsphinx has stopped listening.")
         statusTextView.text = "Pocketsphinx has stopped listening."
     }
     
     func pocketsphinxDidSuspendRecognition() {
-        println("Pocketsphinx has suspended recognition.")
+        print("Pocketsphinx has suspended recognition.")
         statusTextView.text = "Pocketsphinx has suspended recognition."
     }
     
     func pocketsphinxDidResumeRecognition() {
-        println("Pocketsphinx has resumed recognition.")
+        print("Pocketsphinx has resumed recognition.")
         statusTextView.text = "Pocketsphinx has resumed recognition."
     }
     
-    func pocketsphinxDidChangeLanguageModelToFile(newLanguageModelPathAsString: String, newDictionaryPathAsString: String) {
-        println("Pocketsphinx is now using the following language model: \(newLanguageModelPathAsString) and the following dictionary: \(newDictionaryPathAsString)")
+    @objc(pocketsphinxDidChangeLanguageModelToFile:andDictionary:) func pocketsphinxDidChangeLanguageModel(toFile newLanguageModelPathAsString: String, andDictionary newDictionaryPathAsString: String) {
+        print("Pocketsphinx is now using the following language model: \(newLanguageModelPathAsString) and the following dictionary: \(newDictionaryPathAsString)")
     }
     
-    func pocketSphinxContinuousSetupDidFailWithReason(reasonForFailure: String) {
-        println("Listening setup wasn't successful and returned the failure reason: \(reasonForFailure)")
+    func pocketSphinxContinuousSetupDidFail(withReason reasonForFailure: String) {
+        print("Listening setup wasn't successful and returned the failure reason: \(reasonForFailure)")
         statusTextView.text = "Listening setup wasn't successful and returned the failure reason: \(reasonForFailure)"
     }
     
-    func pocketSphinxContinuousTeardownDidFailWithReason(reasonForFailure: String) {
-        println("Listening teardown wasn't successful and returned the failure reason: \(reasonForFailure)")
+    func pocketSphinxContinuousTeardownDidFail(withReason reasonForFailure: String) {
+        print("Listening teardown wasn't successful and returned the failure reason: \(reasonForFailure)")
         statusTextView.text = "Listening teardown wasn't successful and returned the failure reason: \(reasonForFailure)"
     }
     
     func testRecognitionCompleted() {
-        println("A test file that was submitted for recognition is now complete.")
+        print("A test file that was submitted for recognition is now complete.")
         statusTextView.text = "A test file that was submitted for recognition is now complete."
     }
     
     func startListening() {
-        OEPocketsphinxController.sharedInstance().setActive(true, error: nil)
-        OEPocketsphinxController.sharedInstance().startListeningWithLanguageModelAtPath(lmPath, dictionaryAtPath: dicPath, acousticModelAtPath: OEAcousticModel.pathToModel("AcousticModelEnglish"), languageModelIsJSGF: false)
+        do {
+            try OEPocketsphinxController.sharedInstance().setActive(true)
+        } catch {
+            print(error)
+        }
+
+        OEPocketsphinxController.sharedInstance().startListeningWithLanguageModel(atPath: lmPath, dictionaryAtPath: dicPath, acousticModelAtPath: OEAcousticModel.path(toModel: "AcousticModelEnglish"), languageModelIsJSGF: false)
     }
 
     func stopListening() {
@@ -167,7 +166,7 @@ class ViewController: UIViewController, OEEventsObserverDelegate {
     }
     
     func getNewWord() {
-        var randomWord = Int(arc4random_uniform(UInt32(words.count)))
+        let randomWord = Int(arc4random_uniform(UInt32(words.count)))
         currentWord = words[randomWord]
     }
     
@@ -176,14 +175,14 @@ class ViewController: UIViewController, OEEventsObserverDelegate {
         NSLog("Local callback: The user has never set mic permissions or denied permission to this app's mic, so listening will not start.")
         self.startupFailedDueToLackOfPermissions = true
         if OEPocketsphinxController.sharedInstance().isListening {
-            var error = OEPocketsphinxController.sharedInstance().stopListening() // Stop listening if we are listening.
+            let error = OEPocketsphinxController.sharedInstance().stopListening() // Stop listening if we are listening.
             if(error != nil) {
-                NSLog("Error while stopping listening in micPermissionCheckCompleted: %@", error);
+                print("Error while stopping listening in micPermissionCheckCompleted \(error)");
             }
         }
     }
     
-    func pocketsphinxDidReceiveHypothesis(hypothesis: String!, recognitionScore: String!, utteranceID: String!) {
+    func pocketsphinxDidReceiveHypothesis(_ hypothesis: String!, recognitionScore: String!, utteranceID: String!) {
         
         heardTextView.text = "Heard: \(hypothesis)"
     }
